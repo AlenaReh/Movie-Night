70943e49160245a8246d916c24700e7e3cdd4c59
Alena Rehberger <alenarehberger92@gmail.com>
2022-03-31 17:28:46 -0500

    Btn component is created

---
+++ movie-night/src/components/Btn/Btn.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Btn.scss";
+
+const Button = (props) => {
+  return (
+    <button
+      className={`btn ${props.className}`}
+      onClick={props.onClick ? () => props.onCLick() : null}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export const OutlineButton = (props) => {
+    return (
+        <Button
+        className={`btn-outline ${props.className}`}
+        onClick={props.onClick ? () => props.onClick() : null}>
+            {props.children}
+        </Button>
+    );
+};
+
+Button.protoTypes = {
+  onClick: PropTypes.func,
+};
+
+export default Button;
