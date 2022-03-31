226f8c0619428770368a4331476f40354ae4cab0
Alena Rehberger <alenarehberger92@gmail.com>
2022-03-30 21:02:05 -0500

    Added routes

--- movie-night/src/App.js
+++ movie-night/src/App.js
@@ -1,10 +1,25 @@
-import './App.scss';
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./Pages/Home";
+import Catalog from "./Pages/Catalog";
+import Details from "./Pages/Details";
 
 function App() {
   return (
-    <div >
-      This will be my MOVIE app!!!
-    </div>
+    <BrowserRouter>
+
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:category/search/:keyword" element={<Catalog />} />
+          <Route path="/:category" element={<Catalog />} />
+          <Route path="/:category/:id" element={<Details />} />
+        </Routes>
+        <Footer/>
+
+    </BrowserRouter>
   );
 }
 
